469341a79f61e348f327bfa00076e700813704bd
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-07-06 10:25:18 -0700

    Add variable feedforward
    Also renamed project fiel

--- LegalbotShooter/src/main/java/frc/robot/subsystems/Hood.java
+++ LegalbotShooter/src/main/java/frc/robot/subsystems/Hood.java
@@ -29,22 +29,50 @@ public class Hood {
 
     // If the velocity is above the minimum required for movement, the mechanism is moving.
     private boolean isMoving() {
-        return Math.abs(talonSRX.getSelectedSensorVelocity()) >= kStaticFrictionDeadband;
+        return Math.abs(talonSRX.getSelectedSensorVelocity()) > kStaticFrictionDeadband;
     }
 
     public void setAngleMotionMagic(double angle) {
         if (isMoving()) {
             talonSRX.set(ControlMode.MotionMagic, degreesToTicks(angle) + kTicksOffset, 
-            DemandType.ArbitraryFeedForward, HoodConstants.kGravityFF);
+            DemandType.ArbitraryFeedForward, getFFIfMoving());
         } else {
             talonSRX.set(ControlMode.MotionMagic, degreesToTicks(angle) + kTicksOffset,
-            DemandType.ArbitraryFeedForward, HoodConstants.kGravityFF);
+            DemandType.ArbitraryFeedForward, getFFIfNotMoving(angle - getAngle()));
         }
     }
 
-    // Convert degrees to native units
+    public double getAngle() {
+        return ticksToDegrees(talonSRX.getSelectedSensorPosition());
+    }
+
+    /* NOTE: The following utility functions could be moved to a 
+    separate file for organization, but they are placed here for 
+    the purpose of isolating the shooter code from the rest of the 
+    robot in this repository. */
+
+    private double getFFIfMoving() {
+        return HoodConstants.kGravityFF * Math.cos(degreesToRadians(getAngle()));
+    }
+
+    // Use arbitrary FF multiplied by sign of error to hold position in right direction
+    private double getFFIfNotMoving(double error) {
+        double sign = Math.signum(error);
+        return HoodConstants.kGravityFF * Math.cos(degreesToRadians(getAngle()))
+        + sign * HoodConstants.kArbitraryFF;
+    }
+
+    // Convert degrees of arm to native units
     private double degreesToTicks(double degrees) {
         return (degrees / 360) * HoodConstants.kGearRatio * HoodConstants.kEncoderTicksPerRotation;
+    }
+
+    private double ticksToDegrees(double ticks) {
+        return (ticks / HoodConstants.kEncoderTicksPerRotation) * (1 / HoodConstants.kGearRatio) * 360;
+    }
+
+    private double degreesToRadians(double degrees) {
+        return degrees * (Math.PI / 180);
     }
 
 }
